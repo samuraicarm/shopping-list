6af93e4b45c2bcb3615b010aba44f26e13490783
Carmen Dukes <carmendukes@gmail.com>
2020-06-23 22:00:39 -0700

    add jquery events to add and remove items

---
+++ main.js
@@ -0,0 +1,46 @@
+
+'use strict';
+
+// watch form and store value
+function watchForm() {
+    console.log("watch form");
+    $('#js-shopping-list-form').submit(event => {
+    console.log("I get called after the form is submitted.");
+    event.preventDefault();
+    let storeItem = $('#shopping-list-entry').val();
+    console.log(storeItem)
+    addItem(storeItem);
+    });
+   }
+
+// add value to list
+function addItem(storeItem) {
+    $('ul').append(`<li>
+    <span class="shopping-item">${storeItem}</span>
+    <div class="shopping-item-controls">
+      <button class="shopping-item-toggle">
+        <span class="button-label">check</span>
+      </button>
+      <button class="shopping-item-delete">
+        <span class="button-label">delete</span>
+      </button>
+    </div>
+  </li>`)
+}
+
+
+//remove item
+$(function() {
+    $('.shopping-list').on('click', '.shopping-item-delete', function(event) {
+        $(event.target).closest('li').remove();
+    });
+  });
+
+//toggle check
+$(function() {
+    $('.shopping-list').on('click','.shopping-item-toggle', function(event){
+        $(event.target).closest('li').toggleClass('shopping-item__checked');
+      });
+  }); 
+  
+  $(watchForm);
